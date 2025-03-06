2f2d1130a2fa02d50906eaa314360609200e3026
Nandgopal-R <nandgopalrnair@gmail.com>
2025-03-07 01:30:27 +0530

    Add basic favourite page and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import NavBar from './components/NavBar'
 import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom"
+import Favourites from './pages/Favourites'
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourite" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
+
   )
 }
 
---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,8 @@
+function Favourites() {
+  return <div className="favourites-empty">
+    <h2>No Favourite Movie yet</h2>
+    <p>Start adding movies to your favourite and they will appear here</p>
+  </div>
+}
+
+export default Favourites
